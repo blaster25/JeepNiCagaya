0f060c45fb771db868868a127b610bc9e0787123
cxfire16 <cxfire16@gmail.com>
2015-10-02 23:05:21 +0800

    Major Changes
    Revision by JM
    
    Change log :
    - integrated maps API to angular within ionic
    - implemented geolocation with the use of official plugin (still faulty
    but works on browsers
    - fixed errors, see Errors and Fixes file
    - Added findLocation button
    - Added angular test variables (possible issue here)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,41 @@
 angular.module('starter.controllers', [])
 
-.controller('MapCtrl', function($scope) {})
+.controller('MapCtrl', function($scope) {
+ google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(8.4800, 124.6340); // center
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 14,
+            mapTypeControl: false,
+            streetViewControl: false,
+            // mapTypeId: google.maps.MapTypeId.ROADMAP // no need for this I guess?
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);     
+      $scope.homeGPS = function(){
+        var geo_options = {
+                            enableHighAccuracy: true, 
+                            maximumAge        : 30000, 
+                            timeout           : 27000
+                          }; 
+        var onFail = function(){};
+        navigator.geolocation.getCurrentPosition(function(pos,onFail,geo_options) {
+            var xCoords =  new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude); //Get current position
+            $scope.myLoc = xCoords;//bind to scope (displays blank on view)
+            console.log(xCoords);//display to log (works)
+            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            var myLocation = new google.maps.Marker({
+                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+                map: map,
+                title: "My Location"
+            });
+        });
+      };
+});
+  $scope.map = map;
+
+})
 
 .controller('SavedRoutesCtrl', function($scope ) {
   // With the new view caching in Ionic, Controllers are only called
@@ -10,7 +45,7 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-
+   $scope.testVar = "WORKS";
    $scope.SavedRoutes =  [
                         {"title": "San Augustin To Must", 
                         "route": "via Shuttle -> Xavier Heights -> R1",
