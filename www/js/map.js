bb2e591975f4e0c2c5eec6ad221515414ee1f926
cxfire16 <cxfire16@gmail.com>
2015-10-05 03:11:54 +0800

    Fixed merge

--- www/js/map.js
+++ www/js/map.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // var map;
 // function initMap() {
 //   map = new google.maps.Map(document.getElementById('map'), {
@@ -38,45 +38,5 @@
 //   infoWindow.setContent(browserHasGeolocation ?
 //                         'Error: The Geolocation service failed.' :
 //                         'Error: Your browser doesn\'t support geolocation.');
-=======
-// var map;
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: 8.4542, lng: 124.6319},
-//     zoom: 12,
-//   mapTypeControl: false,
-//   streetViewControl: false,
-//   });
 
-// var infoWindow = new google.maps.InfoWindow({map: map});
-
-//   // Try HTML5 geolocation.
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(function(position) {
-//       var pos = {
-//         lat: position.coords.latitude,
-//         lng: position.coords.longitude
-//       };
-
-//       infoWindow.setPosition(pos);
-//       infoWindow.setContent('You are here numbnuts !!.');
-//       map.setCenter(pos);
-//     }, function() {
-//       handleLocationError(true, infoWindow, map.getCenter());
-//     });
-//   } else {
-//     // Browser doesn't support Geolocation
-//     handleLocationError(false, infoWindow, map.getCenter());
-//   }
-
-// // var GeoMarker = new GeolocationMarker(map);
-
-// }
-
-// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-//   infoWindow.setPosition(pos);
-//   infoWindow.setContent(browserHasGeolocation ?
-//                         'Error: The Geolocation service failed.' :
-//                         'Error: Your browser doesn\'t support geolocation.');
->>>>>>> origin/master
 // }
