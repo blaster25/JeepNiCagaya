b48398c57ff902b6ae5ee8da0ad02cb1040761e5
Jebb Matthew Luza <cxfire16@gmail.com>
2015-10-05 02:26:06 +0000

    Geolocation Fixed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,49 @@
 angular.module('starter.controllers', [])
 
-.controller('MapCtrl', function($scope) {})
+.controller('MapCtrl', function($scope) {
+ google.maps.event.addDomListener(window, 'load', function() {
+        var myLatlng = new google.maps.LatLng(8.4800, 124.6340); // center
+ 
+        var mapOptions = {
+              center:             myLatlng,
+              zoom:               14,
+              mapTypeControl:     false,
+              streetViewControl:  false,
+              // mapTypeId: google.maps.MapTypeId.ROADMAP // no need for this I guess?
+            };
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);     
+
+        var geo_options = {
+                      enableHighAccuracy: true, 
+                      maximumAge        : 30000, 
+                      timeout           : 27000
+                          };         
+        
+        document.addEventListener("deviceready", onDeviceReady, false);
+        $scope.homeGPS = onDeviceReady();
+        function onDeviceReady() {navigator.geolocation.getCurrentPosition(onSuccess, onError, geo_options);};
+
+        function onSuccess(position) {
+          var xCoords =  new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          $scope.myLoc = xCoords;
+          console.log(xCoords);
+          map.setCenter(xCoords);
+
+          var myLocation = new google.maps.Marker({
+              position: xCoords,
+              map: map,
+              title: "My Location"
+            });
+          };
+        function onError(error) {
+          console.log("code: " + error.code + "\n message: " + error.message + "\n" );
+          };
+        
+});
+
+   $scope.map = map;
+
+})
 
 .controller('SavedRoutesCtrl', function($scope ) {
   // With the new view caching in Ionic, Controllers are only called
@@ -10,7 +53,7 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-
+   $scope.testVar = "WORKS";
    $scope.SavedRoutes =  [
                         {"title": "San Augustin To Must", 
                         "route": "via Shuttle -> Xavier Heights -> R1",
